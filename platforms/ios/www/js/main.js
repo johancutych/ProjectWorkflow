b43fd8b723fc9dd42202c8dc25b692bde0e878b1
Jan Sedivy <jansedivy@abdoc.net>
2014-12-09 23:01:15 +0100

    cleanup js code little bit

--- platforms/ios/www/js/main.js
+++ platforms/ios/www/js/main.js
@@ -1,111 +1,104 @@
+/* global Hammer */
+
 var running = 0;
 var timer = document.getElementById('timer');
-var test = document.getElementById('test');
-var bgProperty = document.querySelector('.clear');
 var startDate;
-
-function onDeviceReady () {
-window.plugin.notification.local.add({
-                                     id: 2,
-                                     title: 'omo', //Title
-                                     message: 'HEJ JOU', //message
-}, function () { //Callback
-  alert('Added:');
-});
-}
-
 var body = document.querySelector('body');
-var hammertime = new Hammer.Manager(body);
-
-hammertime.add( new Hammer.Swipe({event: 'swipeShortRight', velocity: 0.1, direction: 4}));
-hammertime.add( new Hammer.Swipe({event: 'swipeShortLeft', velocity: 0.1, direction: 2}));
-hammertime.on('swipeShortRight', function(ev) {
-	startPause();
-});
-
-
-function toggleClass() {
-    if (running == 1 || running == 2) {
-
-    }
-}
-
-function startPause() {
-    if (running == 0) {
-        running = 1;
-        startDate = new Date();
-        run();
-    } else if (running == 1) {
-        running = 2;
-        timer.innerHTML = min + ':' + sec;
-    } else if (running == 2) {
-        running = 0;
-        timer.innerHTML = '60:00';
-    }
-}
-
-function formatTime(time) {
-    min = Math.floor((time / 60000) % 60);
-    sec = Math.floor((time / 1000) % 60);
-    mil = Math.floor((time / 10) % 100) ;
-
-    if (sec < 10) {
-        sec = '0' + sec;
-    }
-
-    if (min < 10) {
-        min = '0' + min;
-    }
-
-    return min + ':' + sec;
-}
-
-function calculateDiffDate() {
-    var date = new Date();
-    return date - startDate;
-}
-
-function tick() {
-    if (running == 1) {
-        var time = 0020000 - calculateDiffDate();
-
-        checkingTime(time);
-
-        if (alreadyCheckedEnd == false) {
-            var formated = formatTime(time);
-            render(formated);
-            window.requestAnimationFrame(tick);
-        } else {
-            alreadyCheckedEnd = false;
-            startBreak();
-        }
-    }
-};
 
 var alreadyCheckedWarmUp = false;
 var alreadyCheckedEnd = false;
-function checkingTime(time) {
-    if (time <= 3000000 && !alreadyCheckedWarmUp) {
-        alreadyCheckedWarmUp = true;
-    } else if ( time <= 0 && !alreadyCheckedEnd) {
-        running = 2;
-        alreadyCheckedEnd = true;
+
+var sec, min, ms;
+
+var onDeviceReady = function() {
+  window.plugin.notification.local.add({
+    id: 2,
+    title: 'omo',
+    message: 'HEJ JOU',
+  }, function() {
+    alert('Added:');
+  });
+};
+
+var formatTime = function(time) {
+  min = Math.floor((time / 60000) % 60);
+  sec = Math.floor((time / 1000) % 60);
+  ms = Math.floor((time / 10) % 100) ;
+
+  if (sec < 10) {
+    sec = '0' + sec;
+  }
+
+  if (min < 10) {
+    min = '0' + min;
+  }
+
+  return min + ':' + sec;
+};
+
+var calculateDiffDate = function() {
+  var date = new Date();
+  return date - startDate;
+};
+
+var checkingTime = function(time) {
+  if (time <= 3000000 && !alreadyCheckedWarmUp) {
+    alreadyCheckedWarmUp = true;
+  } else if ( time <= 0 && !alreadyCheckedEnd) {
+    running = 2;
+    alreadyCheckedEnd = true;
+  }
+};
+
+var startBreak = function() {
+  document.addEventListener('deviceready', onDeviceReady(), false);
+};
+
+var render = function(formated) {
+  timer.innerHTML = formated;
+};
+
+var tick = function() {
+  if (running === 1) {
+    var time = 20000 - calculateDiffDate();
+
+    checkingTime(time);
+
+    if (alreadyCheckedEnd === false) {
+      var formated = formatTime(time);
+      render(formated);
+      window.requestAnimationFrame(tick);
+    } else {
+      alreadyCheckedEnd = false;
+      startBreak();
     }
-}
+  }
+};
 
-function render(formated) {
-    timer.innerHTML = formated;
-}
+var run = function() {
+  // TODO(johan): add prefixes
+  window.requestAnimationFrame(tick);
+};
 
-function run() {
-    //TODO(johan): add prefixes
-    window.requestAnimationFrame(tick);
-}
+var startPause = function() {
+  if (running === 0) {
+    running = 1;
+    startDate = new Date();
+    run();
+  } else if (running === 1) {
+    running = 2;
+    timer.innerHTML = min + ':' + sec;
+  } else if (running === 2) {
+    running = 0;
+    timer.innerHTML = '60:00';
+  }
+};
 
-function changeColor(originalValue, newValue, position) {
-    return originalValue + (newValue - originalValue) * position;
-}
+var hammertime = new Hammer.Manager(body);
 
-function startBreak() {
-document.addEventListener('deviceready', onDeviceReady(), false);
-}
+hammertime.add(new Hammer.Swipe({event: 'swipeShortRight', velocity: 0.1, direction: 4}));
+hammertime.add(new Hammer.Swipe({event: 'swipeShortLeft', velocity: 0.1, direction: 2}));
+
+hammertime.on('swipeShortRight', function() {
+  startPause();
+});
